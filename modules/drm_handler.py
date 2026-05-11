87c28569f7ae725d298e7950f970cf7fcdefd3d1
mrmubarikkhan269-arch <mrmubarikkhan269@gmail.com>
2026-05-11 17:35:35 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -243,10 +243,10 @@ async def drm_handler(bot: Client, m: Message):
         if "/Baby" in raw_text7:
             channel_id = m.chat.id
         else:
-            channel_id = raw_text7    
+            channel_id = raw_text7
         await editable.delete()
 
-            elif m.text:
+    elif m.text:
         if any(ext in links[i][1] for ext in [".pdf", ".jpeg", ".jpg", ".png"] for i in range(len(links))):
             raw_text = '1'
             raw_text7 = '/Baby'
