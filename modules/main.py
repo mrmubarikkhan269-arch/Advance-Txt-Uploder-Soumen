b094e041b3dfdb33a0080b444f3c971415142f1b
mrmubarikkhan269-arch <mrmubarikkhan269@gmail.com>
2026-05-11 18:31:55 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -52,7 +52,7 @@ bot = Client(
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.......,.....,
 keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("✨ All bro Commands", callback_data="cmd_command")],
-            [InlineKeyboardButton("💎 All Features", callback_data="feat_command"), InlineKeyboardButton("⚙️ Settings", callback_data="setttings")],
+            [InlineKeyboardButton("💎 All bro Features", callback_data="feat_command"), InlineKeyboardButton("⚙️ Settings", callback_data="setttings")],
             [InlineKeyboardButton("💳 Premium Plans", callback_data="upgrade_command")],
             [InlineKeyboardButton(text="🔍 Developer", url="https://t.me/SmartBoy_ApnaMS"), InlineKeyboardButton(text="👑 Owner", url="https://t.me/MR_Toxic_1")],
         ])      
