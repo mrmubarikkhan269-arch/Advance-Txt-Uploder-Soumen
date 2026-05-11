4553773de02a10136dcaebd78fb1f065a89477f5
mrmubarikkhan269-arch <mrmubarikkhan269@gmail.com>
2026-05-11 16:46:56 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -246,14 +246,22 @@ async def drm_handler(bot: Client, m: Message):
             channel_id = raw_text7    
         await editable.delete()
 
-    elif m.text:
+      elif m.text:
         if any(ext in links[i][1] for ext in [".pdf", ".jpeg", ".jpg", ".png"] for i in range(len(links))):
             raw_text = '1'
             raw_text7 = '/Baby'
             channel_id = m.chat.id
-            b_name = '**Link Input**'
             CR = globals.CR
-            await m.delete()
+            path = os.path.join("downloads", "Free Batch")
+            await editable.edit("**(2). Enter Batch Name or send /Bro\n\nAnd baaki Chize jo Settings\nMe Set hai Wo automatic Lag jaayegi.\n\nJaise ki Credit Name 🌚.\n\nYour Are On Step: 2/2💥**")
+            try:
+                input_bn: Message = await bot.listen(editable.chat.id, filters=filters.text & filters.user(m.from_user.id))
+                raw_text0 = input_bn.text
+                await input_bn.delete(True)
+            except Exception:
+                raw_text0 = '/Bro'
+            b_name = 'Bataya hi Nahi😏😏' if raw_text0 == '/Bro' else raw_text0
+            await editable.delete()
         else:
             editable = await m.reply_text(f"**(1.) Great, Your Link is Captured 🙈\n╭━━━━❰ᴇɴᴛᴇʀ ʀᴇꜱᴏʟᴜᴛɪᴏɴ❱━━➣ \n┣━━⪼ send `144`  for 144p\n┣━━⪼ send `240`  for 240p\n┣━━⪼ send `360`  for 360p\n┣━━⪼ send `480`  for 480p\n┣━━⪼ send `720`  for 720p\n┣━━⪼ send `1080` for 1080p\n╰━━⌈⚡[🦋`{CREDIT}`🦋]⚡⌋━━➣\n\nYour Are On Step: 1/2💥**")
             input2: Message = await bot.listen(editable.chat.id, filters=filters.text & filters.user(m.from_user.id))
@@ -273,11 +281,11 @@ async def drm_handler(bot: Client, m: Message):
                 elif raw_text2 == "720":
                     res = "1280x720"
                 elif raw_text2 == "1080":
-                    res = "1920x1080" 
-                else: 
+                    res = "1920x1080"
+                else:
                     res = "UN"
             except Exception:
-                    res = "UN"
+                res = "UN"
 
             await editable.edit("**(2). Enter Batch Name or send /Bro\n\nAnd baaki Chize jo Settings\nMe Set hai Wo automatic Lag jaayegi.\n\nJaise ki Credit Name 🌚.\n\nYour Are On Step: 2/2💥**")
             try:
